d0f5901c8fda88efc3136a711b6161a0eaebcd42
lilvlad-4life <vladzagrebelnyi122@gmail.com>
2021-12-24 18:09:09 +0200

    767. Reorganize String

--- leetcode.js
+++ leetcode.js
@@ -2420,13 +2420,39 @@ var maxProfit = function (prices) {
  * @param {string} s
  * @return {string}
  */
+
 var reorganizeString = function (s) {
-  // создать объект ключ буква значение
-  // каждым шагом находить букву с наибольшим количевством входов -> конкатенировать -> отнимать в объекте значение
+  let result = "";
+  const letters = [...new Set(s)];
+  const amount = new Array(letters.length).fill(0);
+
+  for (let i = 0; i < s.length; i++) {
+    const elem = s[i];
+    const indexOfElem = letters.indexOf(elem);
+    amount[indexOfElem] += 1;
+  }
+
+  while (result.length !== s.length) {
+    let max = -Infinity;
+    let index;
+    for (let i = 0; i < amount.length; i++) {
+      const element = amount[i];
+      if (
+        element > max &&
+        result[result.length - 1] !== letters[i] &&
+        element !== 0
+      ) {
+        max = element;
+        index = i;
+      }
+    }
+    if (max === -Infinity) {
+      result = "";
+      break;
+    }
+    result += letters[index];
+    amount[index] -= 1;
+  }
+
+  return result;
 };
-
-console.log(reorganizeString("vvvlo"));
-
-// "vvvlo" = "vlvov"
-// "aab" = "bab"
-// "aaab" = ""
