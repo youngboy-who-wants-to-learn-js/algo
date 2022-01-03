03a6b908fcdd2aecf6ccdf0f67b3e0e9c57f2867
lilvlad-4life <vladzagrebelnyi122@gmail.com>
2022-01-03 18:40:35 +0200

    621. Task Scheduler

--- leetcode.js
+++ leetcode.js
@@ -2414,45 +2414,20 @@ var maxProfit = function (prices) {
   */
 };
 
-//! 767. Reorganize String
+//! 621. Task Scheduler
 
-/**
- * @param {string} s
- * @return {string}
- */
+const leastInterval = (tasks, n) => {
+  const newMap = tasks.reduce((map, obj) => {
+    map[obj] = map[obj] + 1 || 1;
+    return map;
+  }, {});
 
-var reorganizeString = function (s) {
-  let result = "";
-  const letters = [...new Set(s)];
-  const amount = new Array(letters.length).fill(0);
+  const array = Object.values(newMap);
+  const maximumRows = Math.max(...array);
+  const lastRowLength = array.filter((x) => x === maximumRows);
 
-  for (let i = 0; i < s.length; i++) {
-    const elem = s[i];
-    const indexOfElem = letters.indexOf(elem);
-    amount[indexOfElem] += 1;
-  }
-
-  while (result.length !== s.length) {
-    let max = -Infinity;
-    let index;
-    for (let i = 0; i < amount.length; i++) {
-      const element = amount[i];
-      if (
-        element > max &&
-        result[result.length - 1] !== letters[i] &&
-        element !== 0
-      ) {
-        max = element;
-        index = i;
-      }
-    }
-    if (max === -Infinity) {
-      result = "";
-      break;
-    }
-    result += letters[index];
-    amount[index] -= 1;
-  }
-
-  return result;
+  return Math.max(
+    tasks.length,
+    (maximumRows - 1) * (n + 1) + lastRowLength.length
+  );
 };
