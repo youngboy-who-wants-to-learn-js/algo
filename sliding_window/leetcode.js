f159083ddf0cbe3194d09699f934e8a026ebc4a7
lilvlad-4life <vladzagrebelnyi122@gmail.com>
2022-01-25 20:12:45 +0200

    904. Fruit Into basketSets

--- sliding_window/leetcode.js
+++ sliding_window/leetcode.js
@@ -47,3 +47,71 @@ const minSubArrayLenExplain = (s, nums) => {
 
   return minLength === Infinity ? 0 : minLength;
 };
+
+//! 904. Fruit Into basketSets
+/**
+ * @param {number[]} fruits
+ * @return {number}
+ */
+var totalFruit = function (fruits) {
+  let leftWindow = 0;
+  let basketSet = new Set();
+  let count = 0;
+  let maximumFruit = 0;
+
+  for (let rightWindow = 0; rightWindow < fruits.length; rightWindow++) {
+    const curElem = fruits[rightWindow];
+    if (
+      basketSet.size > 2 ||
+      (basketSet.size >= 2 && !basketSet.has(curElem))
+    ) {
+      basketSet.clear();
+      count = 0;
+      leftWindow += 1;
+      for (let i = leftWindow; i <= rightWindow; i++) {
+        basketSet.add(fruits[i]);
+        count++;
+      }
+    } else {
+      basketSet.add(curElem);
+      count++;
+      maximumFruit = Math.max(maximumFruit, count);
+    }
+  }
+  return maximumFruit;
+};
+
+/*
+var totalFruitExplain = function (fruits) {
+  let lastFruit = -1;
+  let secondLastFruit = -1;
+  let lastFruitCount = 0;
+  let currentMax = 0;
+  let max = 0;
+
+  for (let i = 0; i < fruits.length; i++) {
+    let fruit = fruits[i];
+
+    if (fruit === lastFruit || fruit === secondLastFruit) {
+      currentMax += 1;
+    } else {
+      currentMax = lastFruitCount + 1;
+    }
+
+    if (fruit === lastFruit) {
+      lastFruitCount += 1;
+    } else {
+      lastFruitCount = 1;
+    }
+
+    if (fruit !== lastFruit) {
+      secondLastFruit = lastFruit;
+      lastFruit = fruit;
+    }
+
+    max = Math.max(currentMax, max);
+  }
+
+  return max;
+};
+*/
